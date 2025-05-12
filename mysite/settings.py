881c25cc241344a72157050177dcec5be979c563
pusilvija <silvijapupsaite@gmail.com>
2025-05-12 15:03:46 +0300

    updated allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['your-planner-production.up.railway.app', 'localhost',]
 
 
 # Application definition
