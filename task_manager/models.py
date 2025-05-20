5dcbb3885ef3065421e024bd0ac744c17875c81c
pusilvija <silvijapupsaite@gmail.com>
2025-05-20 15:21:26 +0300

    added default description

--- task_manager/models.py
+++ task_manager/models.py
@@ -12,7 +12,7 @@ class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tasks")  
     name = models.CharField(max_length=100, blank=True, null=True)
     status = models.CharField(max_length=50, default='to do', blank=True, null=True)
-    description = models.TextField(blank=True, null=True)
+    description = models.TextField(default="No description", blank=True, null=True)
     category = models.CharField(max_length=50, blank=True, null=True)
     order = models.IntegerField(default=0, blank=True, null=True)
 
