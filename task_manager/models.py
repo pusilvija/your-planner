8ac4c5d9375409d2e2335d44d307791d968dd2e8
pusilvija <silvijapupsaite@gmail.com>
2025-06-11 17:08:17 +0300

    minor fix to do -> todo

--- task_manager/models.py
+++ task_manager/models.py
@@ -11,24 +11,9 @@ class Task(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tasks")  
     name = models.CharField(max_length=100, blank=True, null=True)
-    status = models.CharField(max_length=50, default='to do', blank=True, null=True)
+    status = models.CharField(max_length=50, default='todo', blank=True, null=True)
     description = models.TextField(default="No description", blank=True, null=True)
     category = models.CharField(max_length=50, blank=True, null=True)
-
-    # priority
-    # due_date
-    # created_at
-    # updated_at
-    # completed_at
-    # tags
-    # is_recurring
-    # estimated_time
-    # actual_time
-    # dependencies
-    # attachments
-    # comments
-    # archived
-
 
     order = models.IntegerField(default=0, blank=True, null=True)
 
